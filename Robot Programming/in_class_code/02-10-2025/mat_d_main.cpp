58bbb595c6e49daa3f1c06db1d5f0472fac5678f
Marco Casu <mcasu299@gmail.com>
2025-10-08 12:35:07 +0200

    RP code

--- Robot Programming/in_class_code/02-10-2025/mat_d_main.cpp
+++ Robot Programming/in_class_code/02-10-2025/mat_d_main.cpp
@@ -4,4 +4,17 @@ using namespace std;
 
 int main()
 {
+    MatD m(3, 6);
+    MatD m2(6, 3);
+    for (int i = 0; i < 3; ++i)
+    {
+        for (int j = 0; j < 6; ++j)
+        {
+            m.at(i, j) = 0.5;
+            m2.at(j, i) = 2;
+        }
+    }
+    m.printMeBadly(cout);
+    m2.printMeBadly(cout);
+    MatD m3 = m * m2;
 }
--- Robot Programming/in_class_code/02-10-2025/vec_d_main.cpp
+++ Robot Programming/in_class_code/02-10-2025/vec_d_main.cpp
@@ -1,24 +1,9 @@
-#pragma once
 using namespace std;
 #include "vec_d.h"
-#include "mat_d.h"
 #include <cmath>
 
 int main(int argc, char **argv)
 {
-    MatD m(3, 6);
-    int s = m._rows * m._cols;
-    for (int r = 0; r < m._rows; ++r)
-    {
-        for (int c = 0; c < m._cols; ++c)
-        {
-            m.at(r, c) = sin(M_PI / 100 * r) * cos(M_PI / 100 * c);
-        }
-    }
-
-    m.printMeBadly(cout);
-
-    return 0;
     VecD f;
     {
         VecD f2(5);
