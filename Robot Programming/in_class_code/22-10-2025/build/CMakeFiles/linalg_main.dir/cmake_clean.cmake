cfc701ab51c18fb99528f4451a9f7044209fa8ef
Marco Casu <mcasu299@gmail.com>
2025-10-22 12:47:06 +0200

    Add robot programming example and update ML notes
    Added new in-class code example for Robot Programming (22-10-2025) including CMake configuration, source, and build files. Updated Machine Learning LaTeX source to clarify maximum likelihood hypothesis notation and regenerated the PDF.

---
+++ Robot Programming/in_class_code/22-10-2025/build/CMakeFiles/linalg_main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/linalg_main.dir/linalg_main.cpp.o"
+  "CMakeFiles/linalg_main.dir/linalg_main.cpp.o.d"
+  "linalg_main"
+  "linalg_main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/linalg_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Robot Programming/in_class_code/22-10-2025/grid2.cpp
@@ -0,0 +1,76 @@
+#include <cassert>
+#include "opencv2/opencv.hpp"
+
+#include "grid2.h"
+
+Grid::Grid(int r, int c) {
+  if (!r || !c) {
+    _rows=0;
+    _cols=0;
+    _values=0;
+    return;
+  }
+  _rows=r;
+  _cols=c;
+  _values=new CellType[r*c];
+}
+
+Grid::~Grid() {
+  if (!_values)
+    return;
+  delete [] _values;
+}
+  
+Grid::Grid(const Grid& other):
+  Grid(other._rows, other._cols)
+{
+  int s=_rows*_cols;
+  for (int i=0; i<s; ++i)
+    _values[i]=other._values[i];
+}
+
+void Grid::resize(int r, int c) {
+  int other_s=r*c;
+  int s=_rows*_cols;
+  if (other_s!=s) {
+    if (_values) {
+      delete [] _values;
+      _values=0;
+    }
+    if (other_s)
+      _values=new CellType[other_s];
+  }
+  _rows=r;
+  _cols=c;
+}
+
+Grid& Grid::operator=(const Grid& other) {
+  if (&other==this)
+    return *this;
+  resize(other._rows, other._cols);
+  int size=_rows*_cols;
+  for (int i=0; i<size; ++i)
+    _values[i]=other._values[i];
+  return *this;
+}
+
+void Grid::loadFromImage(const char* filename) {
+  std::cerr << "loading [" << filename << "]" << std::endl;
+  cv::Mat img=cv::imread(filename);
+  if (img.rows==0) {
+    throw std::runtime_error("unable to load image");
+  }
+  cv::Mat img_gray;
+  cv::cvtColor(img, img_gray, cv::COLOR_BGR2GRAY);
+  resize(img_gray.rows, img_gray.cols);
+  _rows=img_gray.rows;
+  _cols=img_gray.cols;
+  memcpy(_values, img_gray.data, img_gray.rows*img_gray.cols);
+}
+
+cv::Mat Grid::toCvMat(){
+  cv::Mat m(_rows, _cols, CV_8UC1);
+  memcpy(m.data, _values, _rows * _cols);
+  return m;
+}
+  
